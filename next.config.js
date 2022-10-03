8408382d3a2f6faa104bde4539191c9d3528edb9
harry-21isr <harry21isr@outlook.com>
2022-10-03 14:27:54 -0500

    Final blogPost version

--- next.config.js
+++ next.config.js
@@ -1,7 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+const nextConfig = (phase) => {
+
+	if (phase == PHASE_DEVELOPMENT_SERVER) {
+		return {
+			reactStrictMode: true,
+			swcMinify: true,
+
+			env: {
+				mongodb_username: "harry",
+				mongodb_password: "THEWANTED-20",
+				mongodb_clustername: "cluster0",
+				mongodb_database: "contact",
+			},
+		};
+	}
+  return {
+    env: {
+      mongodb_username: "harry",
+      mongodb_password: "THEWANTED-20",
+      mongodb_clustername: "cluster0",
+      mongodb_database: "contact",
+    },
+  }
+};
+
+/* here dev or production variables will be replace by next js */
+
+module.exports = nextConfig;
